0b972ab1be0b5f14d91c321b8898feba4bbc00b7
Prateek Rungta <prateekrungta@gmail.com>
2023-03-15 20:46:33 +0530

    Add Craft 4 compatibility

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Redirector settings
- * 
+ *
  * @link      https://miranj.in/
  * @copyright Copyright (c) 2021 Miranj Design LLP
  */
@@ -14,41 +14,46 @@ class Settings extends Model
 {
     // Public Properties
     // =========================================================================
-    
+
     /**
      * Handle of field that captures legacy paths
      * @var string
      */
     public $fieldHandle;
-    
+
     /**
      * Strictly match trailing slash in 404 URLs while matching legacy URLs
      * @var bool
      */
     public $matchTrailingSlashes = false;
-    
+
     /**
      * Strictly match query string in 404 URLs while matching legacy URLs
      * @var bool
      */
     public $matchQueryString = false;
-    
+
     /**
      * Append query string to the final redirected URLs
      * @var bool
      */
     public $preserveQueryString = true;
-    
-    
-    
+
     /**
      * @inheritdoc
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             [['fieldHandle'], 'string'],
-            [['matchTrailingSlashes', 'matchQueryString', 'preserveQueryString'], 'boolean'],
+            [
+                [
+                    'matchTrailingSlashes',
+                    'matchQueryString',
+                    'preserveQueryString',
+                ],
+                'boolean',
+            ],
         ];
     }
 }
