fa88fb94702ee56bda3c58cbc1b0f99d97f93880
Prateek Rungta <prateekrungta@gmail.com>
2023-03-16 01:18:58 +0530

    Add controller to configure redirects from routes file

---
+++ src/controllers/RedirectController.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Redirects Controller
+ *
+ * @link      https://miranj.in/
+ * @copyright Copyright (c) 2022 Miranj Design LLP
+ */
+
+namespace miranj\redirector\controllers;
+
+use Craft;
+use craft\web\Controller;
+use craft\web\View;
+use yii\web\BadRequestHttpException;
+use yii\web\Response;
+
+/**
+ * RedirectController
+ */
+class RedirectController extends Controller
+{
+    public function init(): void
+    {
+        $this->allowAnonymous = true;
+        parent::init();
+    }
+
+    /**
+     * @param string $url The redirect URL
+     * @param int $statusCode The response status code
+     * @return Response
+     */
+    public function actionIndex(string $url, int $statusCode = 302): Response
+    {
+        $redirectUrl = $url;
+
+        if ($url) {
+            // treat $url as a Twig object template string and pass it
+            // the route params to render the target URL for the redirect
+            $redirectUrl = Craft::$app
+                ->getView()
+                ->renderObjectTemplate(
+                    $url,
+                    Craft::$app->urlManager->getRouteParams(),
+                    [],
+                    View::TEMPLATE_MODE_SITE,
+                );
+
+            // replace only the matched path with the new target path inside
+            // the *full* original URL so that pagination params are preserved
+            $redirectUrl = str_replace(
+                Craft::$app->request->getPathInfo(),
+                $redirectUrl,
+                Craft::$app->request->fullPath,
+            );
+        }
+
+        if (!$redirectUrl) {
+            throw new BadRequestHttpException('Invalid URL.');
+        }
+
+        return $this->redirect($redirectUrl, $statusCode);
+    }
+}
